a935b2d1ed5dcca3db9d263a4194887cc1941825
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2025-01-23 16:27:43 +0200

    First commit for MyPitch compose

---
+++ app/src/main/java/com/devapps/mypitch/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.devapps.mypitch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.devapps.mypitch.ui.theme.MyPitchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyPitchTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyPitchTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/devapps/mypitch/ui/ScreenDestinations.kt
@@ -0,0 +1,31 @@
+package com.devapps.mypitch.ui
+
+interface ScreenDestination {
+
+    val route: String
+}
+
+object Check : ScreenDestination {
+    override val route = "check_auth"
+}
+
+object Start : ScreenDestination {
+    override val route = "start_screen"
+}
+
+object Home : ScreenDestination {
+    override val route = "home_screen"
+}
+
+object MyPitches : ScreenDestination {
+    override val route = "my_pitches_screen"
+}
+
+object Messages : ScreenDestination {
+    override val route = "messages_screen"
+}
+
+object Profile : ScreenDestination {
+    override val route = "profile_screen"
+}
+
